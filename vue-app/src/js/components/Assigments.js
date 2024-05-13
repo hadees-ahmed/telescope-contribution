e8b882bcb5f226ac5bda858fc9121bf066447963
hadees-ahmed <hadeesahmed@yahoo.com>
2024-05-13 01:05:09 +0100

    create assignment form

--- vue-app/src/js/components/Assigments.js
+++ vue-app/src/js/components/Assigments.js
@@ -5,10 +5,16 @@ export default {
 
     template:
         `
-          <section class="class-y-6">
-          <assigment-list :assignments="filters.incompleteAssignments" status="Inprogress"></assigment-list>
-          <assigment-list :assignments="filters.completeAssignments" status="Completed"></assigment-list>
-          </section>
+          <assigment-list v-if="filters.incompleteAssignments.length >0" :assignments="filters.incompleteAssignments" status="Inprogress"></assigment-list>
+          <assigment-list v-if="filters.completeAssignments.length >0" :assignments="filters.completeAssignments" status="Completed" ></assigment-list>
+          
+          <form class="p-9" @submit.prevent="add">
+            <div class=" border border-grey-600 text-black">
+            <input v-model="newAssignment" placeholder="New Assignemnt..." class="p-2">
+            
+            <button type="submit" class="bg-white p-2 border-l">Add</button>
+            </div>
+          </form>
         `,
 
     data() {
@@ -17,7 +23,9 @@ export default {
                 {name: 'Physics', complete: false, id:1},
                 {name: 'Chemistry', complete: false, id:2},
                 {name: 'Maths', complete: false, id:3}
-            ]
+            ],
+
+            newAssignment: ''
         }
     },
 
@@ -29,6 +37,16 @@ export default {
                 completeAssignments: this.assignments.filter(assignment => assignment.complete)
             }
 
+        }
+    },
+    methods:{
+        add(){
+            this.assignments.push({
+                name: this.newAssignment,
+                complete: false,
+                id: this.assignments.length + 1
+            });
+            this.newAssignment = ''
         }
     }
 }
