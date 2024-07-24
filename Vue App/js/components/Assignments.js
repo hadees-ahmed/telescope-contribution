7593408ed1a3e3d428c00a91cf8abf022c097870
hadees-ahmed <hadeesahmed@yahoo.com>
2024-07-24 14:54:03 +0100

    handle form submission

--- Vue App/js/components/Assignments.js
+++ Vue App/js/components/Assignments.js
@@ -1,10 +1,18 @@
 import AssignmentList from "./AssignmentList.js";
+
 export default {
     template: `
 <assignment-list :assignments="filters.inProgress" title="InProgress"></assignment-list>
 
 <assignment-list :assignments="filters.completed" title="Completed Assignments"></assignment-list>
-</div>`,
+</div>
+
+<form @submit.prevent="add">
+<div class="border border-gray-600">
+<input v-model="newAssignment" placeholder="new assignment ... " >
+<button type="submit">Add</button>
+</div>
+</form>`,
 
     components: {AssignmentList},
 
@@ -14,7 +22,8 @@ export default {
                 {name: 'english', teacher: 'faisal', complete: false, id:1},
                 {name: 'urdu', teacher: 'Ali', complete: false, id:2},
                 {name: 'computer', teacher: 'yasir', complete: false, id:3}
-            ]
+            ],
+            newAssignment:''
         }
     },
 
@@ -24,6 +33,17 @@ export default {
                 inProgress:this.assignments.filter(a => !a.complete),
                 completed:this.assignments.filter(a => a.complete)
             }
+        }
+    },
+
+    methods: {
+        add() {
+            this.assignments.push({
+                name: this.newAssignment,
+                completed: false,
+                id:this.assignments.length + 1
+            })
+            this.newAssignment = ''
         }
     }
 };
